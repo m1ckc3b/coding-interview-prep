b87bb1ea00dce01ee018415e4d35392f3ac107b7
mickceb <cebulski.mi@gmail.com>
2022-01-17 12:14:45 +0100

    init

--- algorithms/norepeats.js
+++ algorithms/norepeats.js
@@ -1,3 +1,10 @@
+/**
+ * Coding Interview Prep #3
+ * Title: No reapts please
+ * Instruction: Return the number of total permutations of the provided string that don't have repeated consecutive letters. 
+ * Assume that all characters in the provided string are each unique.
+ */
+
 
 function permAlone(str) {
   return str;
