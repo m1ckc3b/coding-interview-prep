3e03d0e09f24a7872458f0df133fcd3ac09f6f6b
mickceb <cebulski.mi@gmail.com>
2022-01-17 20:38:57 +0100

    init

---
+++ algorithms/bubblesort.js
@@ -0,0 +1,20 @@
+/**
+ * Coding Interview Prep #5
+ * Title: Implement Bubble sort.
+ * Instruction: Write a function bubbleSort which takes an array of integers as input 
+ * and returns an array of these integers in sorted order from least to greatest.
+ */
+
+
+/**
+ * Return a sorted array without using Array.sort().
+ *
+ * @param {array} array - Given array of numbers.
+ * @return {array} Sorted array
+ */
+function bubbleSort(array) {
+
+  return array
+}
+
+console.log(bubbleSort([1,4,2,8,345,123,43,32,5643,63,123,43,2,55,1,234,92]));
