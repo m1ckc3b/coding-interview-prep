10988711f2febf299fdec45beebb677c83168e03
mickceb <cebulski.mi@gmail.com>
2022-01-18 18:51:33 +0100

    solved

--- data-structures/stack.js
+++ data-structures/stack.js
@@ -8,3 +8,8 @@
 
 var homeworkStack = ["BIO12","HIS80","MAT122","PSY44"];
 // Only change code below this line
+console.log(homeworkStack);
+homeworkStack.pop()
+console.log(homeworkStack);
+homeworkStack.push("CS50")
+console.log(homeworkStack);
