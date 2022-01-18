0b212f7bddb4856e559d606458f93176b692f68e
mickceb <cebulski.mi@gmail.com>
2022-01-18 10:54:30 +0100

    init

---
+++ algorithms/selection sort.js
@@ -0,0 +1,19 @@
+/**
+ * Coding Interview Prep #6
+ * Title: Implement Selection Sort
+ * Instruction: Write a function selectionSort which takes an array of integers as input 
+ * and returns an array of these integers in sorted order from least to greatest.
+ */
+
+/**
+ *
+ *
+ * @param {*} array
+ * @return {*} 
+ */
+function selectionSort(array) {
+
+  return array
+}
+
+console.log(selectionSort([1,4,2,8,345,123,43,32,5643,63,123,43,2,55,1,234,92]));
