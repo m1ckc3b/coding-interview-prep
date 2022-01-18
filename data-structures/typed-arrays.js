d69a4e1f0e926a1559fff23cb4f5a5ce27d36b46
mickceb <cebulski.mi@gmail.com>
2022-01-18 17:48:23 +0100

    init

---
+++ data-structures/typed-arrays.js
@@ -0,0 +1,10 @@
+/**
+ * Coding Interview Prep #11
+ * Category: Data Structures
+ * Title:Typed Arrays
+ * Instructions: First create a buffer that is 64-bytes. 
+ * Then create a Int32Array typed array with a view of it called i32View.
+ */
+
+var buffer;
+var i32View;
