f043544e770347dc83ecc09df50621a665d9df25
mickceb <cebulski.mi@gmail.com>
2022-01-18 11:12:48 +0100

    solved

--- algorithms/insertionSort.js
+++ algorithms/insertionSort.js
@@ -14,6 +14,14 @@
  */
 function insertionSort(array) {
 
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i+1; j <= array.length; j++) {
+      if (array[i] > array[j]) {
+        [array[i], array[j]] = [array[j], array[i]]
+      }
+    }
+  }
+
   return array
 }
 
