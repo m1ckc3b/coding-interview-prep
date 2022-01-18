ba81b56a06cfdc43808476dbad46cbc2f1724bfa
mickceb <cebulski.mi@gmail.com>
2022-01-18 11:02:21 +0100

    init

---
+++ algorithms/insertionSort.js
@@ -0,0 +1,20 @@
+/**
+ * Coding Interview Prep #7
+ * Title: Implement Insertion Sort
+ * Instruction: Write a function insertionSort which takes an array of integers as input 
+ * and returns an array of these integers in sorted order from least to greatest.
+ */
+
+
+/**
+ * Return a given array of numbers sorted without using Array.sort().
+ *
+ * @param {array} array - Given array.
+ * @return {array} array - Sorted array. 
+ */
+function insertionSort(array) {
+
+  return array
+}
+
+console.log(insertionSort([5, 4, 33, 2, 8]))   // should return [2, 4, 5, 8, 33])
