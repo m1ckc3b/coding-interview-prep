2b02213c94ff640a78456d3df180a6803248e55a
mickceb <cebulski.mi@gmail.com>
2022-01-18 11:18:05 +0100

    init

---
+++ algorithms/quickSort.js
@@ -0,0 +1,24 @@
+/**
+ * Coding Interview Prep #8
+ * Title: Implement Quick Sort
+ * Instructions: Write a function quickSort which takes an array of integers as input 
+ * and returns an array of these integers in sorted order from least to greatest.
+ * While the choice of the pivot value is important, any pivot will do for our purposes here. 
+ * For simplicity, the first or last element could be used.
+ */
+
+
+/**
+ * Return a given array of numbers sorted.
+ *
+ * @param {array} array - Given array of numbers.
+ * @return {array} array - Sorted array. 
+ */
+function quickSort(array) {
+
+  return array
+}
+
+console.log(quickSort([1,4,2,8,345,123,43,32,5643,63,123,43,2,55,1,234,92]));
+// should return
+// [1,1,2,2,4,8,32,43,43,55,63,92,123,123,234,345,5643]
