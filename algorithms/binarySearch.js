016215b013e2ea5d4fe2a2e04287ebad5f24fe57
mickceb <cebulski.mi@gmail.com>
2022-01-18 17:30:13 +0100

    solved

--- algorithms/binarySearch.js
+++ algorithms/binarySearch.js
@@ -7,14 +7,44 @@
 
 
 /**
+ * Find a value in a given sorted array.
  *
- *
- * @param {*} searchList
- * @param {*} value
- * @return {*} 
+ * @param {Array} searchList - Given sorted array of numbers.
+ * @param {number} value - Value to find.
+ * @return {array} arrayPath - Return each middle and finale value found. 
  */
 function binarySearch(searchList, value) {
   let arrayPath = [];
+
+  let low = 0
+  let high = searchList.length -1
+  let middle = Math.floor(high / 2)
+
+  if (searchList[middle] === value) {
+    arrayPath.push(searchList[middle])
+    return arrayPath
+  }
+
+  while (searchList[middle] !== value) {
+    arrayPath.push(searchList[middle])
+
+    if (high < low) return "Value Not Found"
+
+    if (searchList[middle] > value) {
+      high = middle - 1
+      middle = low + Math.floor((high - low)/2)
+    } else {
+      low = middle + 1
+      middle = low + Math.floor((high - low)/2)
+    }
+
+    if (searchList[middle] === value) {
+      arrayPath.push(searchList[middle])
+
+      break
+    }
+  }
+
   return arrayPath;
 }
 
@@ -23,10 +53,10 @@ const testArray = [
   23, 49, 70
 ];
 
-
-console.log(binarySearch(testArray, 0));      // should return [13, 5, 2, 0].
-console.log(binarySearch(testArray, 1));      // should return [13, 5, 2, 0, 1].
-console.log(binarySearch(testArray, 2));      // should return [13, 5, 2].
-console.log(binarySearch(testArray, 6));      // should return the string Value Not Found.
-console.log(binarySearch(testArray, 11));     // should return [13, 5, 10, 11].
-console.log(binarySearch(testArray, 13));     // should return [13].
+console.log(binarySearch([1,2,3,4,5,6,7], 5));    // should return [4,6,5]
+console.log(binarySearch(testArray, 0));          // should return [13, 5, 2, 0].
+console.log(binarySearch(testArray, 1));          // should return [13, 5, 2, 0, 1].
+console.log(binarySearch(testArray, 2));          // should return [13, 5, 2].
+console.log(binarySearch(testArray, 6));          // should return the string Value Not Found.
+console.log(binarySearch(testArray, 11));         // should return [13, 5, 10, 11].
+console.log(binarySearch(testArray, 13));         // should return [13].
