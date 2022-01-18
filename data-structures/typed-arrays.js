c88cf56dbd68546d5036a0a1eb8fd05d0729f683
mickceb <cebulski.mi@gmail.com>
2022-01-18 18:48:44 +0100

    solved

--- data-structures/typed-arrays.js
+++ data-structures/typed-arrays.js
@@ -6,5 +6,8 @@
  * Then create a Int32Array typed array with a view of it called i32View.
  */
 
-var buffer;
-var i32View;
+ var buffer= new ArrayBuffer(64)
+ var i32View = new Int32Array(buffer)
+ 
+ console.log(buffer, i32View);
+ console.log(i32View.byteLength);
