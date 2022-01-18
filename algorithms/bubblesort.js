032079794dead657f11e115617576d0fd4dcde9a
mickceb <cebulski.mi@gmail.com>
2022-01-18 10:50:32 +0100

    solved

--- algorithms/bubblesort.js
+++ algorithms/bubblesort.js
@@ -14,7 +14,17 @@
  */
 function bubbleSort(array) {
 
-  return array
+  const newArray = [...array]
+
+  for (let i = 1; i < newArray.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (newArray[j] > newArray[i]) {
+        [newArray[j], newArray[i]] = [newArray[i], newArray[j]]
+      }
+    }
+  }
+  return newArray
 }
 
-console.log(bubbleSort([1,4,2,8,345,123,43,32,5643,63,123,43,2,55,1,234,92]));
+console.log(bubbleSort([1,4,2,8,345,123,43,32,5643,63,123,43,2,55,1,234,92]));    // should return
+// [1,1,2,2,4,8,32,43,43,55,63,92,123,123,234,345,5643]
