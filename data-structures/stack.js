300537ec7835020d76021a0e8570cf5977d35030
mickceb <cebulski.mi@gmail.com>
2022-01-18 18:48:49 +0100

    init

---
+++ data-structures/stack.js
@@ -0,0 +1,10 @@
+/**
+ * Coding Interview Prep #12
+ * Category: Data Structures
+ * Title: Learn how a Stack Works
+ * Instructions: Modify the given array and treat it like a stack using the JavaScript methods mentioned above. 
+ * Remove the top element "PSY44" from the stack. Then add "CS50" to be the new top element of the stack.
+ */
+
+var homeworkStack = ["BIO12","HIS80","MAT122","PSY44"];
+// Only change code below this line
