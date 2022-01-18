3d0eea5ddb98576f63fe5dc3b32430f769c8838e
mickceb <cebulski.mi@gmail.com>
2022-01-18 13:46:48 +0100

    solved

--- algorithms/quickSort.js
+++ algorithms/quickSort.js
@@ -7,18 +7,36 @@
  * For simplicity, the first or last element could be used.
  */
 
-
 /**
- * Return a given array of numbers sorted.
+ * Return given array of numbers sorted recursively.
  *
  * @param {array} array - Given array of numbers.
- * @return {array} array - Sorted array. 
+ * @return {array} Sorted array.
  */
 function quickSort(array) {
 
-  return array
+  if(array.length <= 1){
+    return array;
+  }
+
+  const pivot = array[array.length - 1];
+  const smaller = [];
+  const bigger = [];
+
+  for(let i=0; i < array.length-1;i++){
+    if(array[i] < pivot){
+      smaller.push(array[i]);
+    }
+    else{
+      bigger.push(array[i])
+    }
+  }
+
+  return [...quickSort(smaller) ,pivot,...quickSort(bigger)];
 }
 
+console.log(quickSort([1, 6, 3, 4, 5, 1, 0, 4, 8])); // should return [0, 1, 1, 3, 4, 4, 5, 6, 8]
+console.log(quickSort([5,3,7,6,2,9]));
 console.log(quickSort([1,4,2,8,345,123,43,32,5643,63,123,43,2,55,1,234,92]));
 // should return
 // [1,1,2,2,4,8,32,43,43,55,63,92,123,123,234,345,5643]
