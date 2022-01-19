359f3d85bb409eddbaf767e2cf3747a1f3d0d285
mickceb <cebulski.mi@gmail.com>
2022-01-19 12:51:57 +0100

    init

---
+++ data-structures/circularQueue.js
@@ -0,0 +1,37 @@
+/**
+ * Coding Interview Prep #17
+ * Category: Data Structures
+ * Title: Create a Circular Queue
+ * Instructions: 
+ */
+
+class CircularQueue {
+  constructor(size) {
+
+    this.queue = [];
+    this.read = 0;
+    this.write = 0;
+    this.max = size - 1;
+
+    while (size > 0) {
+      this.queue.push(null);
+      size--;
+    }
+  }
+
+  print() {
+    return this.queue;
+  }
+
+  enqueue(item) {
+    // Only change code below this line
+
+    // Only change code above this line
+  }
+
+  dequeue() {
+    // Only change code below this line
+
+    // Only change code above this line
+  }
+}
